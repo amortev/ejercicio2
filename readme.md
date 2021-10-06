ecca7efd14a150b25654e335fa45ddff18ffbdb4
amorte <amorte@mobilendo.com>
2021-10-06 22:11:33 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,31 @@
+# Ejercicio 2 
+
+## Creación de un formulario de login:
+
+En el formulario se solicitan un email (usuario) y una contraseña, se tendrá que validar que están en el formato correcto, para la contraseña
+solamente se requiere que tenga un mínimo de 5 caracteres.
+
+El botón de login estará siempre activo, y se mostrarán los fallos por pantalla si no están bien cumplimentados los inputs.
+
+## Visualizar y trabajar con la solución del ejercicio en local:
+
+1. Clonar el repositorio
+2. Abrir el proyecto
+3. Abrir la terminal
+4. Instalar depencias:
+
+```javascript
+
+npm install
+
+```
+
+5. Lanzar aplicación para visualizar en el navegador:
+
+```javascript
+
+ionic serve
+
+```
+
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  formLogin: FormGroup;
+
+  constructor(public formBuilder: FormBuilder)  {
+
+  }
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.formLogin = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+    });
+  }
+  login() {
+    switch (true) {
+      case !this.formLogin.controls.email.valid && !this.formLogin.controls.password.valid:
+        alert('El email y la contraseña están en blanco o no tienen un formato válido');
+        break;
+      case !this.formLogin.controls.email.valid:
+        alert( 'El email está en blanco o no tiene un formato válido');
+        break;
+      case!this.formLogin.controls.password.valid:
+        alert('La contraseña está en blanco o no tiene un formato válido, debe contener un mínimo de cinco caracteres');
+        break;
+      case this.formLogin.valid:
+        alert('OK!');
+        break;
+
+    }
+
+  }
 
 }
